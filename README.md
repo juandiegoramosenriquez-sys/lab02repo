cb272305bc315b0b44fb0073a2ba5574a8afcb09
Joel Quijada <joel.quijada@tecsup.edu.pe>
2026-08-28 17:46:58 -0500

    commit 3

---
+++ README.md
@@ -0,0 +1,17 @@
+Registro de Notas - AppFinal.kt
+
+Proyecto simple en Kotlin (consola) para registrar el nombre de un estudiante, la cantidad de cursos que lleva, y el nombre, crédito y costo de cada uno.
+
+Prompts usados
+
+Commit 1:
+
+Crear una app simple en Kotlin que pida el nombre del estudiante y la cantidad de cursos que lleva.
+
+Commit 2:
+
+Agregar un bucle que pregunte el nombre y el crédito de cada curso, según la cantidad ingresada, y calcule el costo automáticamente (crédito x 100).
+
+Commit 3:
+
+Mostrar un resultado final con la tabla de cursos, el total de créditos, el total a pagar, la carga académica y la forma de pago en cuotas.
--- app/src/main/java/com/ramos/ejer02/AppFinal.kt
+++ app/src/main/java/com/ramos/ejer02/AppFinal.kt
@@ -28,7 +28,53 @@ fun main() {
         costos.add(costo)
 
         curso++
+
     }
 
+    println("\nResultado final")
+    println("Estudiante: $nombre")
+    println("\ncurso\tcredito\tcosto")
+
+    var i = 0
+    while (i < cantidadCursos) {
+        println("c${i + 1}\t${creditos[i]}\t${costos[i]}")
+        i++
+    }
+
+    var totalCreditos = 0
+    var totalPagar = 0
+    var j = 0
+
+    while (j < cantidadCursos) {
+        totalCreditos += creditos[j]
+        totalPagar += costos[j]
+        j++
+    }
+
+    val cargaAcademica: String
+
+    if (totalCreditos <= 12) {
+        cargaAcademica = "Normal"
+    } else if (totalCreditos <= 18) {
+        cargaAcademica = "Carga completa"
+    } else {
+        cargaAcademica = "Requiere autorización"
+    }
+
+    val numeroCuotas: Int
+
+    if (totalPagar > 2500) {
+        numeroCuotas = 3
+    } else {
+        numeroCuotas = 2
+    }
+
+    val montoCuota = totalPagar / numeroCuotas
+
+    println("\ncursos: $cantidadCursos")
+    println("total de creditos: $totalCreditos")
+    println("total a pagar: $totalPagar")
+    println("carga academica: $cargaAcademica")
+    println("forma de pago: $numeroCuotas cuotas de $montoCuota cada una")
 
 }
