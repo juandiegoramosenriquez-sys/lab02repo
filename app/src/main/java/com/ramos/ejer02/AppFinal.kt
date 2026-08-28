5419469666363b40aa38690e76043a9d46f4bc04
Joel Quijada <joel.quijada@tecsup.edu.pe>
2026-08-28 17:36:17 -0500

    Initial commit

---
+++ app/src/main/java/com/ramos/ejer02/AppFinal.kt
@@ -0,0 +1,11 @@
+package com.ramos.ejer02
+
+fun main() {
+
+    println("Nombre del estudiante:")
+    val nombre = readLine()
+
+    println("Cantidad de cursos:")
+    val cantidadCursos = readLine()!!.toInt()
+
+}
