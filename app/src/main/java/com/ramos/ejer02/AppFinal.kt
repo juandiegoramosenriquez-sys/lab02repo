f90556765b7b7e935a35ab9afff8bf7bbb5a6b47
Joel Quijada <joel.quijada@tecsup.edu.pe>
2026-08-28 17:40:11 -0500

    commit 2

--- app/src/main/java/com/ramos/ejer02/AppFinal.kt
+++ app/src/main/java/com/ramos/ejer02/AppFinal.kt
@@ -8,5 +8,27 @@ fun main() {
     println("Cantidad de cursos:")
     val cantidadCursos = readLine()!!.toInt()
 
+    val nombresCursos = mutableListOf<String>()
+    val creditos = mutableListOf<Int>()
+    val costos = mutableListOf<Int>()
+
+    var curso = 1
+
+    while (curso <= cantidadCursos) {
+        println("Curso $curso - Nombre:")
+        val nombreCurso = readLine()!!
+
+        println("Curso $curso - Crédito:")
+        val credito = readLine()!!.toInt()
+
+        val costo = credito * 100
+
+        nombresCursos.add(nombreCurso)
+        creditos.add(credito)
+        costos.add(costo)
+
+        curso++
+    }
+
 
 }
